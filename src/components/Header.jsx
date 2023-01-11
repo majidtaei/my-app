20e0efd93e584c2422a12c2fcb11b2aefad0e716
majid <majidtaei4@gmail.com>
2023-01-11 23:40:36 +0330

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useLocation , useNavigate} from "react-router";
+import logo from "../images/logo.png";
+
+export default function Header() {
+  const Loction = useLocation();
+  const navigate = useNavigate()
+  function pathMathRote(route) {
+    if (route === Loction.pathname) {
+      return true;
+    }
+  }
+  return (
+    // HEADER
+    <header className=" bg-[#fdfdfd]  shadow border-b sticky top-0 z-50  ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto ">
+        {/* rightHeader */}
+        <div>
+          <ul className="flex gap-8 items-center text-center justify-center ">
+            <li
+              className={`pb-3 cursor-pointer pt-8 text-sm border-b-[3px]  font-semibold text-gray-400 border-b-transparent ${
+                pathMathRote("/") && " text-black border-b-red-500"
+              }`}
+              onClick={()=> navigate("/")}
+            >
+              خانه
+            </li>
+            <li
+              className={`pb-3 cursor-pointer pt-8 text-sm border-b-[3px]  font-semibold text-gray-400 border-b-transparent ${
+                pathMathRote("/offers") && " text-black border-b-red-500"
+              }`}
+              onClick={()=> navigate("/offers")}
+            >
+              پیشنهاد
+            </li>
+            <li
+              className={`pb-3 cursor-pointer pt-8 text-sm border-b-[3px]  font-semibold text-gray-400 border-b-transparent ${
+                pathMathRote("/sign-in") && " text-black border-b-red-500"
+              }`}
+              onClick={()=> navigate("/sign-in")}
+            >
+              ثبت نام
+            </li>
+          </ul>
+        </div>
+        {/* leftHeader */}
+        <div>
+          <img className="h-12 cursor-pointer" src={logo} alt="logo" onClick={()=> navigate("/")} />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Offers() {
   return (
-    <div className='b bg-black'>Offers</div>
+    <div>Offers</div>
   )
 }
