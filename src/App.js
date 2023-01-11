a9605e53548d19cff7aae4e77221ed2daa97ecb1
majid <majidtaei4@gmail.com>
2023-01-11 17:19:29 +0330

    create pages route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <div>
-      <h1>Hellow world</h1>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Offers() {
+  return (
+    <div className='b bg-black'>Offers</div>
+  )
+}
